452243766503e1bf16f23c4edb8b78e525086ccb
takashi.horiuchi <takashi.horiuchi01@gmail.com>
2022-01-23 12:30:42 +0000

    models fix

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,8 @@ class Micropost < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, length: { maximum: 255 }
+  
+  # お気にり機能の多対多の関係記述
+  has_many :favorites
+  has_many :favorited, through: :favorites, source: :user
 end
